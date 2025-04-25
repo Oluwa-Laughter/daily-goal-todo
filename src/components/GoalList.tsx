ccf74fdf23c27991375cf8a9e6b9ff24b395e30d
Oluwa-Laughter <ismakinde@gmail.com>
2025-04-25 18:54:57 +0100

    added infobox

--- src/components/GoalList.tsx
+++ src/components/GoalList.tsx
@@ -1,5 +1,7 @@
 import Goal from "./Goal";
 import { type Goals } from "../App";
+import InfoBox from "./InfoBox";
+import { type ReactNode } from "react";
 
 type GoalListProps = {
   goals: Goals[];
@@ -7,16 +9,33 @@ type GoalListProps = {
 };
 
 function GoalList({ goals, onDeleteGoal }: GoalListProps) {
+  if (goals.length === 0) {
+    return <InfoBox mode="hint"> You have no goals yet</InfoBox>;
+  }
+
+  let warningBox: ReactNode;
+
+  if (goals.length >= 4) {
+    warningBox = (
+      <InfoBox mode="warning">
+        Too Many Goals. Don't put too much on your plate
+      </InfoBox>
+    );
+  }
+
   return (
-    <ul>
-      {goals.map((goal) => (
-        <li key={goal.id}>
-          <Goal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
-            <p>{goal.description}</p>
-          </Goal>
-        </li>
-      ))}
-    </ul>
+    <>
+      {warningBox}
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <Goal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
+              <p>{goal.description}</p>
+            </Goal>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
---
+++ src/components/InfoBox.tsx
@@ -0,0 +1,24 @@
+import { type ReactNode } from "react";
+
+type InfoBoxProps = {
+  mode: "hint" | "warning";
+  children: ReactNode;
+};
+
+function InfoBox({ mode, children }: InfoBoxProps) {
+  if (mode === "hint")
+    return (
+      <aside className="infobox infobox-hint ">
+        <p>{children}</p>
+      </aside>
+    );
+
+  if (mode === "warning")
+    return (
+      <aside className="infobox infobox-warning warning--medium">
+        <p>{children}</p>
+      </aside>
+    );
+}
+
+export default InfoBox;
